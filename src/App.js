01ce03e1294f3141ede08f497d8e3eb01e1a3d19
iamaryankr <95643709+iamaryankr@users.noreply.github.com>
2024-06-08 14:06:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if(response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://myfacedetectingapp-back-end.onrender.com:3000/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
